a41b068009a65a03ca0326d0b00743ee9e4a7569
N4MERR <namertyzhx@gmail.com>
2026-02-25 19:41:03 +0100

    feat: add vlan configuration section

---
+++ view/device_configuration_views/switch/vlan_view.py
@@ -0,0 +1,34 @@
+"""
+Section for creating and naming VLANs with proper data retrieval.
+"""
+from view.device_configuration_views.base_config_view import BaseConfigView
+from view.device_configuration_views.config_fields import (
+    BaseConfigField, NumberField, DropdownField, ToggleField
+)
+
+class VLANView(BaseConfigView):
+    """
+    Section for creating and naming VLANs.
+    """
+
+    def __init__(self):
+        """
+        Initializes VLAN configuration fields.
+        """
+        super().__init__()
+        vlan_id = self.add_field("vlan_id", NumberField("VLAN ID:"))
+        vlan_id.set_error_message("Enter a valid VLAN ID (1-4094).")
+
+        vlan_name = self.add_field("vlan_name", BaseConfigField("VLAN Name:", is_optional=True))
+        vlan_name.set_error_message("Name cannot be empty if enabled.")
+
+    def get_data(self) -> dict:
+        """
+        Retrieves data for VLAN configuration, ensuring mandatory fields are included.
+        """
+        return {
+            "type": "vlan",
+            "vlan_id": self.fields["vlan_id"].get_value(),
+            "vlan_name": self.fields["vlan_name"].get_value(),
+            "name_enabled": self.fields["vlan_name"].checkbox.isChecked()
+        }
