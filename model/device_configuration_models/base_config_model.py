e644b52cf73506dbd541b7ed86e9ff4b6761b9ff
N4MERR <namertyzhx@gmail.com>
2026-03-01 15:04:01 +0100

    refactor: fix incorrect mvc structure

--- model/device_configuration_models/base_config_model.py
+++ model/device_configuration_models/base_config_model.py
@@ -1,12 +1,16 @@
 class BaseConfigModel:
+    """
+    Base model class handling configuration generation for network devices.
+    """
 
     def __init__(self, session_manager):
+        """
+        Initializes the configuration model with a network session manager.
+        """
         self.session_manager = session_manager
 
     def generate_commands(self, **kwargs) -> list[str]:
+        """
+        Generates the specific Cisco IOS commands required for the configuration.
+        """
         raise NotImplementedError
-
-    def apply_configuration(self, **kwargs):
-        commands = self.generate_commands(**kwargs)
-        if commands:
-            self.session_manager.send_command_set(commands)
