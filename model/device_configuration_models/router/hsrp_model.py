a4a8adad5991d75615a80e4469f1b2b9bb339560
N4MERR <namertyzhx@gmail.com>
2026-03-21 18:30:44 +0100

    update: add ipv6 address option

--- model/device_configuration_models/router/hsrp_model.py
+++ model/device_configuration_models/router/hsrp_model.py
@@ -3,36 +3,37 @@ from model.device_configuration_models.base_interface_model import BaseInterface
 
 class HSRPModel(BaseInterfaceModel):
     """
-    Model for generating Cisco IOS commands for HSRP configuration.
+    Model generating Cisco IOS commands for HSRP, adapting to IPv4 or IPv6 requirements natively.
     """
 
     def generate_commands(self, **kwargs) -> list[str]:
         """
-        Generates commands for creating and configuring HSRP on an interface.
+        Generates configuration commands to establish an HSRP group on a specified interface.
         """
         commands = []
-
         interface = kwargs.get("interface")
-        ip_address = kwargs.get("ip_address")
-        subnet_mask = kwargs.get("subnet_mask")
-        standby_id = kwargs.get("standby_id")
+        group_id = kwargs.get("group_id")
         virtual_ip = kwargs.get("virtual_ip")
+        version = kwargs.get("version")
         priority = kwargs.get("priority")
         preempt = kwargs.get("preempt")
 
-        if interface:
+        if interface and group_id and virtual_ip:
             commands.append(f"interface {interface}")
-            if ip_address and subnet_mask:
-                commands.append(f" ip address {ip_address} {subnet_mask}")
 
-            if standby_id is not None and virtual_ip:
-                commands.append(f" standby {standby_id} ip {virtual_ip}")
-                if priority:
-                    commands.append(f" standby {standby_id} priority {priority}")
-                if preempt == "Enable":
-                    commands.append(f" standby {standby_id} preempt")
+            if version == "ipv6":
+                commands.append("standby version 2")
+                commands.append(f"standby {group_id} ipv6 {virtual_ip}")
+            else:
+                commands.append(f"standby {group_id} ip {virtual_ip}")
 
-            commands.append(" exit")
+            if priority is not None:
+                commands.append(f"standby {group_id} priority {priority}")
+
+            if preempt:
+                commands.append(f"standby {group_id} preempt")
+
+            commands.append("exit")
 
         commands.extend(super().generate_commands(**kwargs))
         return commands
--- view/device_configuration_views/router/hsrp_view.py
+++ view/device_configuration_views/router/hsrp_view.py
@@ -1,22 +1,21 @@
-from view.device_configuration_views.base_config_view import BaseConfigView
-from view.device_configuration_views.input_fields.base_input_field import BaseInputField
-from view.device_configuration_views.input_fields.ip_address_field import IPAddressField
-from view.device_configuration_views.input_fields.subnet_mask_field import SubnetMaskField
-from view.device_configuration_views.input_fields.dropdown_field import DropdownField
-from view.device_configuration_views.input_fields.ranged_number_field import RangedNumberField
 from PySide6.QtWidgets import QPushButton
 from PySide6.QtCore import Signal
+from view.device_configuration_views.base_config_view import BaseConfigView
+from view.device_configuration_views.input_fields.dropdown_field import DropdownField
+from view.device_configuration_views.input_fields.number_field import NumberField
+from view.device_configuration_views.input_fields.adaptive_ip_address_field import AdaptiveIPAddressField
+from view.device_configuration_views.input_fields.toggle_field import ToggleField
 
 
 class HSRPView(BaseConfigView):
     """
-    View handling HSRP configuration for router interfaces.
+    View handling dual-stack HSRP (Hot Standby Router Protocol) configuration.
     """
     load_interfaces_signal = Signal()
 
     def __init__(self):
         """
-        Initializes the interface dropdown, IP configuration, and HSRP specific fields.
+        Initializes HSRP configuration UI components including an adaptive Virtual IP field.
         """
         super().__init__()
 
@@ -29,52 +28,33 @@ class HSRPView(BaseConfigView):
         self.load_interfaces_btn.clicked.connect(self.load_interfaces_signal.emit)
         self.button_layout.insertWidget(0, self.load_interfaces_btn)
 
-        self.interface_dropdown = DropdownField("Interface:", [], is_optional=False)
-        self.add_field("interface", self.interface_dropdown)
+        self.add_field("interface", DropdownField("Target Interface:", [], is_optional=False))
+        self.add_field("group_id", NumberField("Standby Group ID:", is_optional=False))
 
-        self.add_field("ip_address", IPAddressField("Interface IP Address:", is_optional=True))
-        self.add_field("subnet_mask", SubnetMaskField("Interface Subnet Mask:", is_optional=True))
+        self.virtual_ip_field = AdaptiveIPAddressField("Virtual IP Address:", is_optional=False)
+        self.add_field("virtual_ip", self.virtual_ip_field)
 
-        self.add_field("standby_id", RangedNumberField("Standby ID (0-4095):", 0, 4095, is_optional=False))
-        self.add_field("virtual_ip", IPAddressField("Virtual IP Address:", is_optional=False))
-        self.add_field("priority", RangedNumberField("Priority (0-255):", 0, 255, is_optional=True))
-        self.add_field("preempt", DropdownField("Preempt:", ["Disable", "Enable"], is_optional=True))
-
-        self.fields["subnet_mask"].radio.setEnabled(False)
-        self.fields["ip_address"].radio.toggled.connect(self._sync_subnet_mask_state)
-
-        self._sync_subnet_mask_state(self.fields["ip_address"].radio.isChecked())
-
-    def _sync_subnet_mask_state(self, checked: bool):
-        """
-        Synchronizes the state of the subnet mask field with the IP address field.
-        """
-        self.fields["subnet_mask"].input_widget.setEnabled(checked)
-        self.fields["subnet_mask"].radio.setChecked(checked)
-        if not checked:
-            self.fields["subnet_mask"].input_widget.clear()
-
-    def get_data(self) -> dict:
-        """
-        Retrieves HSRP configuration data.
-        """
-        ip_enabled = self.fields["ip_address"].radio.isChecked()
-
-        return {
-            "type": "hsrp_configuration",
-            "interface": self.fields["interface"].get_value(),
-            "ip_address": self.fields["ip_address"].get_value() if ip_enabled else "",
-            "subnet_mask": self.fields["subnet_mask"].get_value() if ip_enabled else "",
-            "standby_id": self.fields["standby_id"].get_value(),
-            "virtual_ip": self.fields["virtual_ip"].get_value(),
-            "priority": self.fields["priority"].get_value() if self.fields["priority"].radio.isChecked() else "",
-            "preempt": self.fields["preempt"].get_value() if self.fields["preempt"].radio.isChecked() else "Disable",
-            "_save_configuration": self.save_configuration_cb.isChecked()
-        }
+        self.add_field("priority", NumberField("Priority (0-255):", is_optional=True))
+        self.add_field("preempt", ToggleField("Enable Preempt:", is_optional=True))
 
     def update_interfaces(self, interfaces: list[str]):
         """
-        Populates the interface dropdown with retrieved device data.
+        Updates the interface dropdown with retrieved device data.
         """
-        self.interface_dropdown.input_widget.clear()
-        self.interface_dropdown.input_widget.addItems(interfaces)
+        self.fields["interface"].input_widget.clear()
+        self.fields["interface"].input_widget.addItems(interfaces)
+
+    def get_data(self) -> dict:
+        """
+        Retrieves HSRP configuration data including the detected IP protocol version.
+        """
+        return {
+            "type": "hsrp",
+            "interface": self.fields["interface"].get_value(),
+            "group_id": self.fields["group_id"].get_value(),
+            "virtual_ip": self.virtual_ip_field.get_value(),
+            "version": self.virtual_ip_field.get_ip_version(),
+            "priority": self.fields["priority"].get_value() if self.fields["priority"].radio.isChecked() else None,
+            "preempt": self.fields["preempt"].get_value() if self.fields["preempt"].radio.isChecked() else None,
+            "_save_configuration": self.save_configuration_cb.isChecked()
+        }
