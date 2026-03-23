0fb391cc42e5d36e330630474418a7c0ac3911d2
N4MERR <namertyzhx@gmail.com>
2026-03-23 15:17:38 +0100

    refactor: move nat_load_worker to load_workers module

--- controller/tab_controllers/device_configuration_controllers/nat_controller.py
+++ controller/tab_controllers/device_configuration_controllers/nat_controller.py
@@ -1,35 +1,6 @@
 from PySide6.QtCore import QThread, Signal
 from controller.tab_controllers.device_configuration_controllers.base_config_controller import BaseConfigController
-
-
-class NatLoadWorker(QThread):
-    """
-    Asynchronous worker for fetching NAT-specific device data (ACLs and Pools).
-    """
-    finished_signal = Signal(str, list)
-
-    def __init__(self, model, data_type):
-        """
-        Initializes the worker with the model and the specific data category to fetch.
-        """
-        super().__init__()
-        self.model = model
-        self.data_type = data_type
-
-    def run(self):
-        """
-        Executes retrieval logic and ensures signals are emitted even on failure.
-        """
-        try:
-            data = []
-            if self.data_type == "acls":
-                data = self.model.get_acls()
-            elif self.data_type == "pools":
-                data = self.model.get_pools()
-
-            self.finished_signal.emit(self.data_type, data if data is not None else [])
-        except Exception:
-            self.finished_signal.emit(self.data_type, [])
+from controller.tab_controllers.device_configuration_controllers.load_workers import NatLoadWorker
 
 
 class NATTranslationRuleController(BaseConfigController):
